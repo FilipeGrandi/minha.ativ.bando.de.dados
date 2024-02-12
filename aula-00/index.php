6e4bc94e2604f600a273585aba507da69ba8bdc0
Alberane <alberanelucio@gmail.com>
2024-02-12 16:33:14 -0300

    Index - Aula 00

---
+++ aula-00/index.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Aula 00</title>
+</head>
+<body>
+<h1>Aula 00</h1>
+<a href="../index.php">< Aulas</a>
+</body>
+</html>
