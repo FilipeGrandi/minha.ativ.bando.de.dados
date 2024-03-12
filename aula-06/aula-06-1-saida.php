764ea8ed6b6818c46da5e6dc9b41776c0233b373
Alberane <alberanelucio@gmail.com>
2024-03-12 18:28:05 -0300

    Aulas Editadas

--- aula-06/aula-06-1-saida.php
+++ aula-06/aula-06-1-saida.php
@@ -5,3 +5,9 @@
 echo "Fala aí rapaziada!!!";
 
 print("Pode ser assim também");
+
+$variavel = 55;
+var_dump($variavel);
+
+$modelosDeCarros = array("Sandero", "Gol", "Belina", "Fusca");
+var_dump($variavel);
