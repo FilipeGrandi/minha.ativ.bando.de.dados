73dbac933cdcb3e19d2b72e373d947d9abe37408
Alberane <alberanelucio@gmail.com>
2024-03-14 14:10:25 -0300

    Aulas Update

---
+++ index.php
@@ -0,0 +1,41 @@
+<!doctype html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+
+<?php
+/*
+ *  Esse arquivo index.php faz a listagem das pastas encontradas na rais do projeto
+ *  e as mostra em html usando links para acessos às pastas  
+ */
+
+// Diretório a ser listado
+$diretorio = "./";
+
+// Abrir o diretório
+$dh = opendir($diretorio);
+
+// Se o diretório foi aberto com sucesso
+if ($dh) {
+    // Ler cada entrada do diretório
+    while (($arquivo = readdir($dh)) !== false) {
+        // Verificar se é um diretório e não é um diretório oculto
+        if (is_dir($diretorio . $arquivo) && !preg_match('/^\./', $arquivo)) {
+            // Exibir o nome do diretório
+            echo "<a href=\"$arquivo\">$arquivo</a><br>";
+        }
+    }
+
+    // Fechar o diretório
+    closedir($dh);
+} else {
+    // Erro ao abrir o diretório
+    echo "Erro ao abrir o diretório: $diretorio";
+}
+?>
+
+</body>
+</html>
